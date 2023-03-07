f4d083e32458ed303c32012ff1be3c79cef76e82
Hotshot824 <shen485@gmail.com>
2023-03-07 19:37:28 +0800

    add: Led control.

--- UART_Serial/boardA/boardA.ino
+++ UART_Serial/boardA/boardA.ino
@@ -1,33 +1,90 @@
+#define LED_FLASH_INTERVAL 500
+#define RED_LED 2
+#define GREEN_LED 3
+
 void setup() {
   // opens serial port, set data rate to 9600 bps
-  Serial.begin(9600);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
   Serial1.begin(9600);
+  Serial.begin(9600); 
+}
+
+void led_on(int turn)
+{
+  switch (turn) {
+    case 0:
+      digitalWrite(RED_LED, HIGH);
+      digitalWrite(GREEN_LED, LOW);
+      break;
+    case 1:
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(GREEN_LED, HIGH);
+      break;
+    default:
+      break;
+  }
+}
+
+void led_off()
+{
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
 }
 
 void loop() {
-  static char buf[200];
+  static char *buf = NULL;
   static int  length = 0;
-  buf[length] = '\0';
   int incoming_byte; 
 
-  if(Serial.available() > 0) {
+  static unsigned long last_time_led;
+  static bool toggle_on = false;
+  static int turn = 0; 
+
+  while(Serial.available() > 0) {
     incoming_byte = Serial.read();
+    buf = (char*)realloc(buf, length + 1);
     buf[length++] = incoming_byte;
     if(incoming_byte == '\n') { 
       buf[length] = '\0';
       Serial.print(buf);
-      Serial1.write(buf);
+      Serial1.print(buf);
+      free(buf);
+      buf = NULL;
       length = 0;
     }
   }
-  
-  if (Serial1.available() > 0) {
+
+  while(Serial1.available() > 0) {
     incoming_byte = Serial1.read();
+    buf = (char*)realloc(buf, length + 1);
     buf[length++] = incoming_byte;
     if(incoming_byte == '\n') {
       buf[length] = '\0';
-      Serial.print(buf);
-      length = 0;
     }
+  }
+
+  if (length > 0) {
+    if(strcmp(buf, "ON\n") == 0) {
+      last_time_led = millis();
+      led_on(turn);
+      turn = (turn+1)%2;
+      Serial.println("LED ON");
+      toggle_on = true;
+    }
+    if(strcmp(buf, "OFF\n") == 0) {
+      led_off();
+      Serial.println("LED OFF");
+      toggle_on = false;
+    }
+    free(buf);
+    buf = NULL;
+    length = 0;
+  }
+
+  if(toggle_on && millis()-last_time_led >= LED_FLASH_INTERVAL) {
+    last_time_led = millis();
+    led_on(turn);
+    turn = (turn+1)%2;
   }
 }
--- UART_Serial/boardB/boardB.ino
+++ UART_Serial/boardB/boardB.ino
@@ -1,32 +1,37 @@
 void setup() {
   // opens serial port, set data rate to 9600 bps
-  Serial.begin(9600);
   Serial1.begin(9600);
+  Serial.begin(9600); 
 }
 
 void loop() {
-  static char buf[200];
+  static char *buf = NULL;
   static int  length = 0;
-  buf[length] = '\0';
   int incoming_byte; 
 
-  if(Serial.available() > 0) {
+  while(Serial.available() > 0) {
     incoming_byte = Serial.read();
+    buf = (char*)realloc(buf, length + 1);
     buf[length++] = incoming_byte;
     if(incoming_byte == '\n') { 
       buf[length] = '\0';
       Serial.print(buf);
-      Serial1.write(buf);
+      Serial1.print(buf);
+      free(buf);
+      buf = NULL;
       length = 0;
     }
   }
-  
-  if (Serial1.available() > 0) {
+
+  while(Serial1.available() > 0) {
     incoming_byte = Serial1.read();
+    buf = (char*)realloc(buf, length + 1);
     buf[length++] = incoming_byte;
     if(incoming_byte == '\n') {
       buf[length] = '\0';
       Serial.print(buf);
+      free(buf);
+      buf = NULL;
       length = 0;
     }
   }
